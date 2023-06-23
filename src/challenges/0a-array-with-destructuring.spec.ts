82b7acd13a9fb3ffea1a18da88337526d779513a
cynthiacodes <cjnkstudy01@hotmail.com>
2023-06-23 12:49:31 +0100

    refactored using destructing and created test file

---
+++ src/challenges/0a-array-with-destructuring.spec.ts
@@ -0,0 +1,7 @@
+import { firstMeal, secondMeal, thirdMeal } from "./0a-array-with-destructuring";
+
+test("meal variables have correct values", () => {
+  expect(firstMeal).toBe("Breakfast");
+  expect(secondMeal).toBe("Lunch");
+  expect(thirdMeal).toBe("Dinner");
+});
