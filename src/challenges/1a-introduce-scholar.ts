6217ba0c02dbb0a6b55d7e34d16aca1d462e25a7
cynthiacodes <cjnkstudy01@hotmail.com>
2023-06-23 13:56:19 +0100

    used parameter destructuring

--- src/challenges/1a-introduce-scholar.ts
+++ src/challenges/1a-introduce-scholar.ts
@@ -4,11 +4,18 @@ interface Scholar {
   cohortNumber: number;
 }
 
-function introduceScholar(scholar: Scholar): string {
-  const {firstName, lastName, cohortNumber} = scholar;
-  const message = `${firstName} ${lastName} is an Academy Scholar, on Cohort ${cohortNumber}.`;
-  // use object destructuring, for practice!
-  return message;
+//parameter destructuring
+
+function introduceScholar(scholar: Scholar) {
+  return `${scholar.firstName} ${scholar.lastName} is an Academy Scholar, on Cohort ${scholar.cohortNumber}.`;
 }
+
+
+// function introduceScholar(scholar: Scholar): string {
+//   const {firstName, lastName, cohortNumber} = scholar;
+//   const message = `${firstName} ${lastName} is an Academy Scholar, on Cohort ${cohortNumber}.`;
+//   // use object destructuring, for practice!
+//   return message;
+// }
 
 export default introduceScholar;
