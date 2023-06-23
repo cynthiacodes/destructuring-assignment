af8db55e2984ddbdd0f91dafd7c620b73f7cce3c
cynthiacodes <cjnkstudy01@hotmail.com>
2023-06-23 14:05:45 +0100

    used parameter destructuring

--- src/challenges/1b-list-followers.ts
+++ src/challenges/1b-list-followers.ts
@@ -1,10 +1,14 @@
+// parameter destructuring
 
-function listFollowers(followerNames: string[]): string {
-  // complete function
-  // use array destructuring, for practice!
-  const [firstFollower, secondFollowers] = followerNames;
-  const followerMessage = `Followed by ${firstFollower}, ${secondFollowers} and others`
-  return followerMessage;
+function listFollowers([firstFollower, secondFollowers]: string[]): string {
+  return `Followed by ${firstFollower}, ${secondFollowers} and others`;
 }
+
+// function listFollowers(followerNames: string[]): string {
+//   use array destructuring, for practice!
+//   const [firstFollower, secondFollowers] = followerNames;
+//   const followerMessage = `Followed by ${firstFollower}, ${secondFollowers} and others`
+//   return followerMessage;
+// }
 
 export default listFollowers;
