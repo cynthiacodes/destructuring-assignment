0d4a4dde0a3a532aabae50b4e23fcdf50ee7a798
cynthiacodes <cjnkstudy01@hotmail.com>
2023-06-23 13:01:34 +0100

    addded destructing and test to object challenge

---
+++ src/challenges/0b-object-with-destructuring.spec.ts
@@ -0,0 +1,7 @@
+import { abbreviation, capital, population } from "./0b-object-with-destructuring";
+
+test("country variables have correct values", () => {
+  expect(abbreviation).toBe("UK");
+  expect(capital).toBe("London");
+  expect(population).toBe(67000000);
+});
